9c7f981eaa0941ee0592d114332311a03e4a99fe
Sonny Piers <sonny@fastmail.net>
2016-06-12 17:25:08 +0200

    ltx 2.4.2

--- packages/client-register/src/index.js
+++ packages/client-register/src/index.js
@@ -1,33 +1,38 @@
-'use strict'
-
-const ltx = require('ltx')
-
 /* References
  *  - In-Band Registration https://xmpp.org/extensions/xep-0077.html
  *  - prosody mod_register https://prosody.im/doc/modules/mod_register
  */
-function register (creds, cb) {
-  const iq = ltx`
+
+export const NS = 'jabber:iq:register'
+export const NS_FEATURE = 'http://jabber.org/features/iq-register'
+export const NS_DISCO_INFO = 'http://jabber.org/protocol/disco#info'
+
+export function register (client, creds, cb) {
+  return client.request((
     <iq type='set'>
-      <query xmlns='jabber:iq:register'>
-        <username>${creds.username}</username>
-        <password>${creds.password}</password>
+      <query xmlns={NS}>
+        <username>{creds.username}</username>
+        <password>{creds.password}</password>
       </query>
     </iq>
-  `
-
-  return this.request(iq, cb)
+  ), cb)
 }
 
-register.supported = function (element) {
+export function isSupported (element) {
   if (element.is('stream:features')) {
-    return !!element.getChild('register', 'http://jabber.org/features/iq-register')
+    return !!element.getChild('register', NS_FEATURE)
   }
 
-  const query = element.getChild('query', 'http://jabber.org/protocol/disco#info')
-  return element.is('iq') && query && query.getChild('register', 'jabber:iq:register')
+  const query = element.getChild('query', NS_DISCO_INFO)
+  return element.is('iq') && query && query.getChild('register', NS)
 }
 
-module.exports = (client) => {
-  client.register = register
+export function registerClient (...args) {
+  register(this, ...args)
 }
+
+export function plugin (client) {
+  client.register = registerClient
+}
+
+export default plugin
--- packages/xml/src/index.js
+++ packages/xml/src/index.js
@@ -1,23 +1,5 @@
 import ltx from 'ltx'
 
-const Element = ltx.Element
-
-// TODO ltx PR for null/undefined children
-ltx.createElement = function createElement (name, attrs /*, child1, child2, ...*/) {
-  var el = new Element(name, attrs)
-
-  for (var i = 2; i < arguments.length; i++) {
-    const child = arguments[i]
-    if (child instanceof Element || typeof child === 'string') {
-      el.cnode(child)
-    }
-  }
-
-  return el
-}
-
-export {Element}
-
-export const {parse} = ltx
+export const {parse, Element} = ltx
 
 export default ltx
